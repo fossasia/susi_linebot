9ae8dae62ee712c6a4af0c93d4f6e10d9ec981d3
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-07-23 15:10:30 +0500

    Added Carousels for RSS type response

--- index.js
+++ index.js
@@ -64,25 +64,96 @@ function handleEvent(event) {
         } else if (type.length == 3 && type[2].type == "map") {
             var lat = type[2].latitude;
             var lon = type[2].longitude;
-            var address = JSON.parse(body1)).answers[0].data[0].place
+            var address = JSON.parse(body1).answers[0].data[0].place
 
-            const answer1 = [
-			{
-                type: 'text',
-                text: ans
-            },
-			{
-                "type": "location",
-                "title": "Location",
-                "address": address,
-                "latitude": lat,
-                "longitude": lon
-            }
-			]
+            const answer = [{
+                    type: 'text',
+                    text: ans
+                },
+                {
+                    "type": "location",
+                    "title": "Location",
+                    "address": address,
+                    "latitude": lat,
+                    "longitude": lon
+                }
+            ]
 
             // use reply API
-            return client.replyMessage(event.replyToken, answer1);
+            return client.replyMessage(event.replyToken, answer);
 
+        } else if (type.length == 1 && type[0].type == "table") {
+            var data = JSON.parse(body1).answers[0].data;
+            var columns = type[0].columns;
+            var key = Object.keys(columns);
+            var msg = [];
+            console.log(key);
+
+            for (var i = 0; i < 5; i++) {
+                msg[i] = "";
+                msg[i] = {
+                    type: 'text',
+                    text: key[0].toUpperCase() + ": " + data[i][key[0]] + "\n" + key[1].toUpperCase() + ": " + data[i][key[1]] + "\n" + key[2].toUpperCase() + ": " + data[i][key[2]]
+                }
+            }
+            return client.replyMessage(event.replyToken, msg);
+        } else if (type.length == 2 && type[1].type == "rss") {
+            var data = JSON.parse(body1).answers[0].data;
+            var columns = type[1];
+            var key = Object.keys(columns);
+            var msg, title, link, query;
+            var carousel = [];
+            console.log(key);
+
+            for (var i = 1; i < 4; i++) {
+                title = key[1].toUpperCase() + ": " + data[i][key[1]];
+                query = title;
+                msg = key[2].toUpperCase() + ": " + data[i][key[2]];
+                link = data[i][key[3]]
+                if (title.length >= 40) {
+                    title = title.substring(0, 36);
+                    title = title + "...";
+                }
+
+                if (msg.length >= 60) {
+                    msg = msg.substring(0, 56);
+                    msg = msg + "...";
+                }
+
+                carousel[i] = {
+                    "title": title,
+                    "text": msg,
+                    "actions": [{
+                            "type": "uri",
+                            "label": "View detail",
+                            "uri": link
+                        },
+                        {
+                            "type": "message",
+                            "label": "Ask SUSI again",
+                            "text": query
+                        }
+                    ]
+                };
+            }
+            const answer = [{
+                    type: 'text',
+                    text: ans
+                },
+                {
+                    "type": "template",
+                    "altText": "this is a carousel template",
+                    "template": {
+                        "type": "carousel",
+                        "columns": [
+                            carousel[1],
+                            carousel[2],
+                            carousel[3]
+                        ]
+                    }
+                }
+            ]
+            return client.replyMessage(event.replyToken, answer);
         }
 
     })
