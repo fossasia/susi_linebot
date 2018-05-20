b9cf96421af61667eb277043ecde9c3077dccda2
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-05-20 12:19:24 +0530

    Correct webhook url

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const client = new line.Client(config);
 const app = express();
 
 // register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/', line.middleware(config), (req, res) => {
+app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then((result) => res.json(result))
